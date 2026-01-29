f0b8ef6752169338c4708bbb95ed5b54d935b949
Deep Roy <dr1454253@gmail.com>
2026-01-29 12:43:40 +0530

    Styled FetchOld component and updated UI

--- src/pages/FetchOld.jsx
+++ src/pages/FetchOld.jsx
@@ -1,5 +1,36 @@
-export const FetchOld = ()=>{
-    return(
-        <h1>Welcome FetchOld</h1>
-    )
-}
+import { useEffect, useState } from "react";
+import { fetchPost } from "../API/Api";
+
+export const FetchOld = () => {
+  const [post, setPosts] = useState([]);
+
+  const getPostData = async () => {
+    try {
+      const res = await fetchPost();
+      res.status === 200 ? setPosts(res.data) : [];
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPostData();
+  }, []);
+
+  return (
+    <section className="posts-container">
+      <h1 className="page-title">Traditional Fetch Posts</h1>
+
+      <ul className="posts-grid">
+        {post.map((curPost) => {
+          return (
+            <li className="post-card" key={curPost.id}>
+              <h2>{curPost.title}</h2>
+              <p>{curPost.body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+};
